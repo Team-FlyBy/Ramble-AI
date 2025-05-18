59a4448d068b62f6b07fb795bdbc551fbc71cbc5
jiwan <wldhks1120@naver.com>
2025-05-18 15:13:15 +0900

    fix: update model paths in config.py #2

--- STT/VOSK/config.py
+++ STT/VOSK/config.py
@@ -1,4 +1,7 @@
 SAMPLE_RATE = 16000
 
-KO_MODEL_PATH = "/Users/jiwan/Documents/GitHub/Remble-AI/STT/VOSK/model/vosk-model-small-ko-0.22"
-EN_MODEL_PATH = "/Users/jiwan/Documents/GitHub/Remble-AI/STT/VOSK/model/vosk-model-small-en-us-0.15"
+KO_MODEL_PATH = r"D:\Python\Remble-AI\STT\VOSK\vost_models\vosk-model-small-ko-0.22"
+# EN_MODEL_PATH = r"D:\Python\Remble-AI\STT\VOSK\vost_models\vosk-model-small-en-us-0.15"
+# EN_MODEL_PATH = r"D:\Python\Remble-AI\STT\VOSK\vost_models\vosk-model-en-us-0.22-lgraph"
+EN_MODEL_PATH = r"D:\Python\Remble-AI\STT\VOSK\vost_models\vosk-model-en-us-0.22"
+
